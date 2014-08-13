8ac9593654c60d3228bee476b544ffcdcb5bcbc6
hkhk4862 <hkhk4862@naver.com>
2014-08-13 22:23:33 +0900

    하경적용

--- SensitiveProject/src/com/example/sensitive/CameraActivity.java
+++ SensitiveProject/src/com/example/sensitive/CameraActivity.java
@@ -31,38 +31,39 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback {
 	VideoView mVideo;
 	private static int fileIndex = 0;
 	private Camera mCamera;
-	
+
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE); // 상단 타이틀바 없애기
 		setContentView(R.layout.activity_camera);
-		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); // 가로 화면 고정
-		
-		mPreview = (SurfaceView)findViewById(R.id.surface);
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); 
+
+		mPreview = (SurfaceView) findViewById(R.id.surface);
 		mHolder = mPreview.getHolder();
 		mHolder.addCallback(this);
-		mVideo = (VideoView)findViewById(R.id.videoview);
-		mPlayBtn = (Button)findViewById(R.id.playBtn);
-		userId = (TextView)findViewById(R.id.userId);
-		
+		mVideo = (VideoView) findViewById(R.id.videoview);
+		mPlayBtn = (Button) findViewById(R.id.playBtn);
+		userId = (TextView) findViewById(R.id.userId);
+
 		Intent intent = getIntent();
 		String test = intent.getStringExtra("userId");
 		if (test != null) {
 			userId.setText(test);
 		}
-		
+
 		mPreview.setVisibility(View.VISIBLE);
 		mVideo.setVisibility(View.INVISIBLE);
-		
+
 		mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-		
+
 		mPlayBtn.setOnClickListener(new Button.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				
+
 				if (mIsStart == false) {
-					String sd = Environment.getExternalStorageDirectory().getAbsolutePath();
+					String sd = Environment.getExternalStorageDirectory()
+							.getAbsolutePath();
 					fileIndex++;
 					Path = sd + "/video" + fileIndex + ".mp4";
 					if (mCamera != null) {
@@ -78,23 +79,29 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback {
 					}
 					mRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
 					mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-					mRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-					mRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.MPEG_4_SP);
-					mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+					mRecorder
+							.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+					mRecorder
+							.setVideoEncoder(MediaRecorder.VideoEncoder.MPEG_4_SP);
+					mRecorder
+							.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
 					mRecorder.setOutputFile(Path);
 					mRecorder.setPreviewDisplay(mHolder.getSurface());
-					
+
 					try {
 						mRecorder.prepare();
 						mRecorder.start();
 					} catch (IllegalStateException e) {
-						Toast.makeText(CameraActivity.this, e.toString(), 1).show();
+						Toast.makeText(CameraActivity.this, e.toString(), 1)
+								.show();
 						return;
 					} catch (IOException e) {
-						Toast.makeText(CameraActivity.this, e.toString(), 1).show();
+						Toast.makeText(CameraActivity.this, e.toString(), 1)
+								.show();
 						return;
 					} catch (Exception e) {
-						Toast.makeText(CameraActivity.this, e.toString(), 1).show();
+						Toast.makeText(CameraActivity.this, e.toString(), 1)
+								.show();
 						return;
 					}
 					mIsStart = true;
@@ -107,9 +114,10 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback {
 					mIsStart = false;
 					Toast.makeText(CameraActivity.this, "저장되었습니다.", 1).show();
 					mPlayBtn.setText("Start");
+
 				}
 			}
-			
+
 		});
 	}
 
@@ -118,7 +126,7 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback {
 		try {
 			// 카메라 객체를 만든다
 			mCamera = Camera.open();
-			
+
 			Camera.Parameters parameters = mCamera.getParameters();
 			parameters.setRotation(90);
 			mCamera.setParameters(parameters);
@@ -138,26 +146,28 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback {
 	}
 
 	@Override
-	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
 		// TODO Auto-generated method stub
-				// 서피스 변경되었을 때의 대응 루틴
-				if (mCamera != null) {
-					Camera.Parameters parameters = mCamera.getParameters();
-					// 프리뷰 사이즈 값 재조정
-					parameters.setPreviewSize(width, height);
-					mCamera.setParameters(parameters);
-					// 프리뷰 다시 시작
-					mCamera.startPreview();
-				}
+		// 서피스 변경되었을 때의 대응 루틴
+		if (mCamera != null) {
+			Camera.Parameters parameters = mCamera.getParameters();
+			// 프리뷰 사이즈 값 재조정
+			parameters.setPreviewSize(width, height);
+			mCamera.setParameters(parameters);
+			// 프리뷰 다시 시작
+			mCamera.startPreview();
+		}
 	}
 
 	@Override
 	public void surfaceDestroyed(SurfaceHolder holder) {
 		// TODO Auto-generated method stub
 	}
+
 	public void onDestroy() {
 		super.onDestroy();
-		
+
 		if (mCamera != null) {
 			mCamera.stopPreview();
 			// 카메라 객체 초기화
